7c21b707171fb4664d9c36accf1fd67270c518d0
Pavel G. Koukoushkin <k.pavel.g@gmail.com>
2010-09-21 16:13:39 +0400

    devel

--- func.lisp
+++ func.lisp
@@ -1,5 +1,7 @@
 (defpackage :oci8-sugar
-  (:use :oci8 :cl))
+  (:use :oci8 :cl)
+  (:export :define-sql-command
+           :select-all))
 
 (in-package :oci8-sugar)
 
@@ -15,9 +17,17 @@
             vars))
     (nreverse vars)))
 
+(defun columns-lengths (statement)
+  (statement-execute statement *service-context* :mode :describe-only)
+  (loop for i from 1 to (attr-get statement :attr-param-count :unsigned-short)
+     for param = (param-get statement i)
+     collect
+       (attr-get param :attr-data-size :short)))
+
 (defmacro define-sql-command (name (statement (&rest bind-sqlt-type-sizes)
-                                              (&rest define-sqlt-type-sizes)))
-  (alexandria:with-unique-names (stmthp binds sqlt type size tmp-func defs call-iteration)
+                                              (&rest define-sqlt-type-sizes)
+                                              &key default-iterator))
+  (alexandria:with-unique-names (stmthp binds tmp-func defs call-iteration)
     (let ((vars (sql-variables statement)))
       `(let ((,stmthp (create-statement ,statement))
              (,binds (make-array (length ',vars))))
@@ -25,9 +35,11 @@
          ,@(loop for var in vars
               for sqlt-type-size in bind-sqlt-type-sizes
               for i from 1 collect
-                `(destructuring-bind (,sqlt ,type &optional ,size)
-                     ',sqlt-type-size
-                   (setf (elt ,binds ,(1- i)) (bind-by-pos ,stmthp ,i ,type ,sqlt :size ,size))))
+                (destructuring-bind (sqlt type &optional size)
+                    sqlt-type-size
+                  `(setf (elt ,binds ,(1- i))
+                         (bind-by-pos ,stmthp ,i ,type ,sqlt
+                                      :size ,size))))
          (defun ,(if define-sqlt-type-sizes
                      tmp-func
                      name)
@@ -35,17 +47,24 @@
            ,@(loop for i from 1
                 for var in vars collect
                   `(setf (pretty-data (elt ,binds ,(1- i))) ,var))
-           (statement-execute ,stmthp *service-context* :iters 1 :mode :commit-on-success))
+           (statement-execute ,stmthp *service-context*
+                              :iters ,(if define-sqlt-type-sizes
+                                          0
+                                          1)
+                              :mode :commit-on-success))
          ,(when define-sqlt-type-sizes
                 `(let ((,defs (make-array (length ',define-sqlt-type-sizes))))
                    ,@(loop for define-sqlt-type-size in define-sqlt-type-sizes
                         for i from 1
                         collect
-                          `(destructuring-bind (,sqlt ,type &optional ,size)
-                               ',define-sqlt-type-size
-                               (setf (elt ,defs ,(1- i))
-                                     (define-by-pos ,stmthp ,i ,type ,sqlt :size ,size))))
-                   (defun ,name ,(append vars '(&key) '(iterate-function))
+                          (destructuring-bind (sqlt type &optional size)
+                              define-sqlt-type-size
+                            `(setf (elt ,defs ,(1- i))
+                                   (define-by-pos ,stmthp ,i ,type ,sqlt
+                                                  :size ,(if (eq :auto size)
+                                                            `(nth (1- ,i) (columns-lengths ,stmthp))
+                                                            size)))))
+                   (defun ,name ,(append vars '(&key) `((iterate-function ,default-iterator)))
                      (,tmp-func ,@vars)
                      (flet ((,call-iteration ()
                               (funcall iterate-function
@@ -56,4 +75,15 @@
                            (loop do
                                 (statement-fetch ,stmthp)
                                 (,call-iteration))
-                         (no-data () (,call-iteration)))))))))))
+                         (no-data ()
+                           (when (plusp (attr-get ,stmthp :attr-rows-fetched :unsigned-char))
+                             (,call-iteration))))))))))))
+
+(defmacro select-all (func-name &rest bind-args)
+  (alexandria:with-unique-names (result)
+    `(let ((,result))
+       (,func-name ,@bind-args :iterate-function
+                   (lambda (&rest data)
+                     (push data
+                           ,result)))
+       (nreverse ,result))))
